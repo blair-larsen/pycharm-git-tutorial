26b8b272932c2de489fcdb0184d8d2ec4b58501b
blair-larsen <blair.larsen@hotmail.com>
2022-05-24 12:33:20 -0700

    new function

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def my_function(a,b):
+    return a + b
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Blair')
     print("where do you live")
 
+
+    print(my_function(8, 8))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
